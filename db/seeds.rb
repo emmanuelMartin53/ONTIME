b580b80d3579a995e75acdc0832d721907ec58c0
charlie gardere <charlie.gardere@gmail.com>
2025-06-02 16:38:19 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts"task created"
 
 
 puts "Starting flight creation"
-@flight = Flight.new(user: User.first , flight_number:"TU629", airport:"bordeaux", destination: "tunis", takeoff_time:DateTime.new(2025, 6, 4, 1, 45), landing_time:DateTime.new(2025, 6, 4, 6, 45), user_departure_address:"15 rue des doris 33130 Bègles")
+@flight = Flight.new(user: User.first , flight_number:"AF7437", airport:"bordeaux", destination: "tunis", takeoff_time:DateTime.new(2025, 6, 4, 1, 45), landing_time:DateTime.new(2025, 6, 4, 6, 45), user_departure_address:"15 rue des doris 33130 Bègles")
 @arrayTask = @flight.user.tasks.where(flight_id: nil)
 
 @arrayTask.each do |task|
