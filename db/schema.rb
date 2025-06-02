a5a0448a39922b541e72a501f18972cdfc8f3d3f
charlie gardere <charlie.gardere@gmail.com>
2025-06-02 16:45:14 +0200

    categorie added to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_02_123844) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_02_144303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_02_123844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "flight_id"
+    t.string "categorie"
     t.index ["flight_id"], name: "index_tasks_on_flight_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
