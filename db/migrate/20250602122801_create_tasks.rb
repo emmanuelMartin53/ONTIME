56809107587bb0d39ba3a581af9a8e609b2c7ff1
charlie gardere <charlie.gardere@gmail.com>
2025-06-02 14:33:27 +0200

    table crées

---
+++ db/migrate/20250602122801_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :flight, null: false, foreign_key: true
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20250602123023_create_flights.rb
@@ -0,0 +1,17 @@
+class CreateFlights < ActiveRecord::Migration[7.1]
+  def change
+    create_table :flights do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :task, null: false, foreign_key: true
+      t.string :flight_number
+      t.string :airport
+      t.string :terminal
+      t.string :destination
+      t.date :takeoff_time
+      t.date :landing_time
+      t.string :user_departure_address
+
+      t.timestamps
+    end
+  end
+end
