5b78c53ca00d2af72f82c6e4ca0a3e091fadadc2
florent.bedubourg@supdevinci-edu.fr <florent.bedubourg@supdevinci-edu.fr>
2025-06-05 17:08:16 +0200

    s

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_06_05_133208) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -117,10 +115,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_05_133208) do
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "alerts", "flights"
   add_foreign_key "alerts", "users"
   add_foreign_key "flights", "users"
