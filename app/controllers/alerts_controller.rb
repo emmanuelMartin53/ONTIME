6f2d42a59e91ace85570b96a3d33474b1c8d3f47
Emmanuel Martin <em.martin456@gmail.com>
2025-06-06 12:11:37 +0200

    update

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -17,7 +17,7 @@ class AlertsController < ApplicationController
 
   def create
 
-    @alert = Alert.new(alerts_params)
+    @alert = Alert.new(alert_params)
     @alert.flight = Flight.find(params[:flight_id])
     @alert.user = current_user
     @flight = Flight.find(params[:flight_id])
@@ -99,7 +99,7 @@ class AlertsController < ApplicationController
 
     private
 
-  def alerts_params
+  def alert_params
     params.require(:alert).permit(:content)
   end
 
