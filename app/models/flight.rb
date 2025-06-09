c5e5130fde51a3a94408fec06b6180de6b0696ef
florent.bedubourg@supdevinci-edu.fr <florent.bedubourg@supdevinci-edu.fr>
2025-06-09 10:08:56 +0200

    fr choix locomotion

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -6,4 +6,15 @@ class Flight < ApplicationRecord
   has_one_attached :photo
 
   enum :mobility_choice, [ :walking, :driving, :transit, :bicycling ]
+
+  MOBILITY_LABELS = {
+    "walking" => "À pied",
+    "driving" => "Voiture",
+    "transit" => "Transports en commun",
+    "bicycling" => "Vélo"
+  }
+
+  def mobility_choice_label
+    MOBILITY_LABELS[mobility_choice]
+  end
 end
