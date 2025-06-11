f2c90800762c98ff85c334f4e8977bb24305ade0
florent.bedubourg@supdevinci-edu.fr <florent.bedubourg@supdevinci-edu.fr>
2025-06-11 11:41:01 +0200

    futur vol ou pas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
-    @flights = Flight.where(user: @user).all
+    @flights = Flight.where(user: @user).where('takeoff_time > ?', Time.current)
     @flight = @flights.order(:takeoff_time).first
     @tasks = Task.where(taskable: @user)
   end
